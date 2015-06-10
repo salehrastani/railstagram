9863fc1746116ad0df35d57f3455003974e6b2c4
Saleh Rastani <salehrastani@gmail.com>
2015-06-10 15:54:51 -0700

    add http request to instagram and display result of request

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ActionController::Base
-  def index
 
+  # include HTTParty
+
+  def index
+    @images = []
+    response = HTTParty.get('https://api.instagram.com/v1/tags/snow/media/recent?access_token=578084450.1fb234f.c3375e7a981b4199a4342f481c7c395d')
+    for image in response["data"]
+      @images << image["images"]["standard_resolution"]["url"]
+    end
   end
+
 end
